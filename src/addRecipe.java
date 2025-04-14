92fa2079a15fa75c50d988c0622f68f47b81f4ad
akula-mako <alexbarbov7@gmail.com>
2025-04-14 16:48:55 +0300

    before the addRecipe backend is developed (in case of error lol)

--- src/addRecipe.java
+++ src/addRecipe.java
@@ -1,2 +1,17 @@
+import javax.swing.*;
+
 public class addRecipe {
+    private JLabel recipeLabel;
+    private JButton recipeButton;
+    private JButton aboutButton;
+    private JButton homeButton;
+    private JTextField recipeNameField;
+    private JLabel recipeNameLabel;
+    private JLabel iconLabel;
+    private JLabel ingredientsLabel;
+    private JTextArea ingredientsTextArea;
+    private JLabel instructionsLabel;
+    private JTextArea instructionsTextArea;
+    private JLabel tagsLabel;
+    private JTextArea tagsTextArea;
 }
